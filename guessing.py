43122cbbaa8fc6451825895dc9b3de60f94bdd4f
annabellee <anlee@potomacschool.org>
2021-11-18 09:20:33 -0500

    game

--- guessing.py
+++ guessing.py
@@ -17,3 +17,38 @@ Mina = True
 Sam = True
 Will = True
 Yumn = True
+
+def guessChecker(list)
+    for x in list:
+        if "Alex" in x:
+            Alex is False
+        if "Aneesh" in x:
+            Aneesh is False
+        if "Annabel" in x:
+            Annabel is False
+        if "Dashiell" in x:
+            Dashiell is False
+        if "Ellie" in x:
+            Ellie is False
+        if "Evie" in x:
+            Evie is False
+        if "Grant" in x:
+            Grant is False
+        if "Hudson" in x:
+            Hudson is False
+        if "Jack" in x:
+            Jack is False
+        if "Jonas" in x:
+            Jonas is False
+        if "Kate" in x:
+            Kate is False
+        if "Kayla" in x:
+            Kayla is False
+        if "Mina" in x:
+            Mina is False
+        if "Sam" in x:
+            Sam is False
+        if "Will" in x:
+            Will is False
+        if "Yumn" in x:
+            Yumn is False
